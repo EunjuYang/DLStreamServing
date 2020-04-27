29ff4c7df06372bb2db8de3b3e9b9d79e8a7e2b4
ejyang <yejyang@kaist.ac.kr>
2020-04-27 16:46:03 +0900

    After meeting

--- online-dl/onlinedl/utils.py
+++ online-dl/onlinedl/utils.py
@@ -11,14 +11,14 @@ class ModelManager:
         self.stub = chunk_pb2_grpc.FileServerStub(channel)
         self.model_name = model_name
 
-    def upload_model(self, file_path):
+    def upload_model(self, file_path, loss):
         """
         client library to upload model file
         :param file_path: file path to upload
         :param model_name: name of model
         :return:
         """
-        chunks_generator = self.get_file_chunks(file_path, self.model_name)
+        chunks_generator = self._get_file_chunks(file_path, self.model_name, loss)
         response = self.stub.upload_model(chunks_generator)
         assert response.length == os.path.getsize(file_path)
 
@@ -28,27 +28,27 @@ class ModelManager:
         :param download_path: file path to save the file
         :return:
         """
-        response = self.stub.download_model(chunk_pb2.Request(name=model_name))
+        response = self.stub.download_model(chunk_pb2.Request(name=self.model_name))
         if response == None:
             print("[Error] No model name with %s" %self.model_name)
             return False
-        self.save_chunks_to_file(response, download_path)
+        self._save_chunks_to_file(response, download_path)
         return True
 
-    def save_chunks_to_file(self, chunks, filename=None):
+    def _save_chunks_to_file(self, chunks, filename=None):
         with open(filename, 'wb') as f:
             for chunk in chunks:
                 f.write(chunk.buffer)
         return filename
 
-    def get_file_chunks(self, filename, model_name=None):
+    def _get_file_chunks(self, filename, model_name=None, loss):
 
         with open(filename, 'rb') as f:
             while True:
                 piece = f.read(CHUNK_SIZE);
                 if len(piece) == 0:
                     return
-                yield chunk_pb2.Chunk(name=model_name, buffer=piece)
+                yield chunk_pb2.Chunk(name=model_name, buffer=piece, loss=loss)
 
 
 
