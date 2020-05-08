dfa3b34c2b3e9dccba62a846f0eabc730dc76a3c
ChanghaLee <iop851@kaist.ac.kr>
2020-05-08 19:24:56 +0900

    Need to initiate "post" variable. -ChanghaLee

--- testbench/create-data-mongo/run.py
+++ testbench/create-data-mongo/run.py
@@ -10,11 +10,11 @@ port = os.environ['MONGO_PORT_27017_TCP_PORT']
 client = MongoClient(ip, int(port))
 db = client['inference'] # inference database
 collection = db[os.environ['NAME']] # model name
-post = {}
 
 while True:
     # The number of AMI ID is 5
     for i in range(5):
+        post = {}
         batch_size = random.randint(1,10) # batch size is variant.
         post['amiid'] = i
         post['pred'] = np.random.random(batch_size).tolist() # shape is (batch,)
