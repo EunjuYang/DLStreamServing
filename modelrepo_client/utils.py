7da484766f4da77765518a70a2c8305cd6da4e58
Eunju <yejyang@kaist.ac.kr>
2020-06-08 20:01:05 +0900

    fix exist_loss --> loss

--- modelrepo_client/utils.py
+++ modelrepo_client/utils.py
@@ -10,19 +10,19 @@ class ModelRepoClient:
         channel = grpc.insecure_channel(address)
         self.stub = chunk_pb2_grpc.FileServerStub(channel)
 
-    def upload_model(self, model_name, file_path):
+    def upload_model(self, model_name, file_path, loss):
 
-        chunks_generator = self._get_file_chunks(file_path, model_name)
+        chunks_generator = self._get_file_chunks(file_path, model_name, loss)
         response = self.stub.upload_model(chunks_generator)
         assert response.length == os.path.getsize(file_path)
 
-    def download_model(self, download_path):
+    def download_model(self, download_path, loss=100000):
         """
         client library to download model file
         :param download_path: file path to save the file
         :return:
         """
-        response = self.stub.download_model(chunk_pb2.Request(name=self.model_name))
+        response = self.stub.download_model(chunk_pb2.Request(name=self.model_name, loss=loss))
         if response == None:
             print("[Error] No model name with %s" %self.model_name)
             return False
