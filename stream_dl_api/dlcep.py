acbb44628ba1340ba831dbcf69253987f30715fb
ChanghaLee <iop851@kaist.ac.kr>
2020-06-16 18:17:35 +0900

    Bug fix, related to InferenceDL's dlcep. -ChanghaLee

--- stream_dl_api/dlcep.py
+++ stream_dl_api/dlcep.py
@@ -241,7 +241,7 @@ class IncStreamDLStub(StreamDLStub):
         y_shape = (amount, self.lf_size)
         x_batch = np.zeros(shape=x_shape, dtype=self.dtype)
         y_batch = np.zeros(shape=y_shape, dtype=self.dtype)
-        id_batch = np.zeros(shape=(self.batch_size, 1), dtype=np.int32)
+        id_batch = np.zeros(shape=(amount, 1), dtype=np.int32)
 
         for i in range(amount):
             x_batch[i] = self.buffer[0][:self.lb_size]
